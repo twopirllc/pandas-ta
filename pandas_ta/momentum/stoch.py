3d97965749a38de42fbc3318c52bcef68312da1a
fff-git <fsdfgs@gmx.com>
2020-05-29 17:08:28 +0200

    correction retour ligne dans stoch

--- pandas_ta/momentum/stoch.py
+++ pandas_ta/momentum/stoch.py
@@ -48,7 +48,6 @@ def stoch(high, low, close, fast_k=None, slow_k=None, slow_d=None, offset=None, 
     fastd.name = f"STOCHFd_{slow_d}"
     slowk.name = f"STOCHk_{slow_k}"
     slowd.name = f"STOCHd_{slow_d}"
-
     fastk.category = fastd.category = slowk.category = slowd.category = 'momentum'
 
     # Prepare DataFrame to return
