0d7489b915ef0f2bc2aa3e293a8d2b134bad4caf
Kevin Johnson <appliedmathkj@gmail.com>
2021-03-09 13:06:55 -0800

    ENH #242 #243 trade_return trade_offset

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
         "pandas_ta.volatility",
         "pandas_ta.volume"
     ],
-    version=".".join(("0", "2", "48b")),
+    version=".".join(("0", "2", "49b")),
     description=long_description,
     long_description=long_description,
     author="Kevin Johnson",
