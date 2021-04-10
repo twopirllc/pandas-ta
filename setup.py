3222001ea799d6fb207d7e4ea861856a709fde5a
Kevin Johnson <appliedmathkj@gmail.com>
2021-04-10 09:53:13 -0700

    BUG #265 fix

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
         "pandas_ta.volatility",
         "pandas_ta.volume"
     ],
-    version=".".join(("0", "2", "65b")),
+    version=".".join(("0", "2", "66b")),
     description=long_description,
     long_description=long_description,
     author="Kevin Johnson",
