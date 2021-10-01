0ac21189f7d055dedb61927f390f68405a61a6f0
Kevin Johnson <appliedmathkj@gmail.com>
2021-10-01 10:10:47 -0700

    DOC performance category doc refactor

--- pandas_ta/performance/percent_return.py
+++ pandas_ta/performance/percent_return.py
@@ -3,7 +3,33 @@ from pandas_ta.utils import get_offset, verify_series
 
 
 def percent_return(close, length=None, cumulative=None, offset=None, **kwargs):
-    """Indicator: Percent Return"""
+    """Percent Return
+
+    Calculates the percent return of a Series.
+    See also: help(df.ta.percent_return) for additional **kwargs a valid 'df'.
+
+    Sources:
+        https://stackoverflow.com/questions/31287552/logarithmic-returns-in-pandas-dataframe
+
+    Calculation:
+        Default Inputs:
+            length=1, cumulative=False
+        PCTRET = close.pct_change(length)
+        CUMPCTRET = PCTRET.cumsum() if cumulative
+
+    Args:
+        close (pd.Series): Series of 'close's
+        length (int): It's period. Default: 20
+        cumulative (bool): If True, returns the cumulative returns. Default: False
+        offset (int): How many periods to offset the result. Default: 0
+
+    Kwargs:
+        fillna (value, optional): pd.DataFrame.fillna(value)
+        fill_method (value, optional): Type of fill method
+
+    Returns:
+        pd.Series: New feature generated.
+    """
     # Validate Arguments
     length = int(length) if length and length > 0 else 1
     cumulative = bool(cumulative) if cumulative is not None and cumulative else False
@@ -33,33 +59,3 @@ def percent_return(close, length=None, cumulative=None, offset=None, **kwargs):
     pct_return.category = "performance"
 
     return pct_return
-
-
-percent_return.__doc__ = \
-"""Percent Return
-
-Calculates the percent return of a Series.
-See also: help(df.ta.percent_return) for additional **kwargs a valid 'df'.
-
-Sources:
-    https://stackoverflow.com/questions/31287552/logarithmic-returns-in-pandas-dataframe
-
-Calculation:
-    Default Inputs:
-        length=1, cumulative=False
-    PCTRET = close.pct_change(length)
-    CUMPCTRET = PCTRET.cumsum() if cumulative
-
-Args:
-    close (pd.Series): Series of 'close's
-    length (int): It's period. Default: 20
-    cumulative (bool): If True, returns the cumulative returns. Default: False
-    offset (int): How many periods to offset the result. Default: 0
-
-Kwargs:
-    fillna (value, optional): pd.DataFrame.fillna(value)
-    fill_method (value, optional): Type of fill method
-
-Returns:
-    pd.Series: New feature generated.
-"""
