6b8738a626a957f8767dea4d6c26a60aa9a85702
Ralph Vogl <neutro2@outlook.de>
2022-09-20 20:34:19 +0200

    d/0012 - corrected year division factor

--- pandas_ta/utils/_time.py
+++ pandas_ta/utils/_time.py
@@ -82,7 +82,7 @@ def total_time(df: DataFrame, tf: str = "years") -> float:
     Useful for annualization."""
     time_diff = df.index[-1] - df.index[0]
     TimeFrame = {
-        "years": time_diff.days / 365.25,
+        "years": time_diff.days / 365.242199074074074,
         "months": time_diff.days / 30.417,
         "weeks": time_diff.days / 7,
         "days": time_diff.days,
