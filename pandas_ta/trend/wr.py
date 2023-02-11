db2955f15da553cc1c5c261c26eb8eca0847963d
andymc31 <72295085+andymc31@users.noreply.github.com>
2023-02-11 13:53:58 -0500

    Update wr.py

--- pandas_ta/trend/wr.py
+++ pandas_ta/trend/wr.py
@@ -1,30 +1,47 @@
-# -*- coding: utf-8 -*-
 """
 WR Williams Overbought/Oversold Index
-TODO: change input to rows and not entire dataframe
+
 
 """
+import pandas as pd
+from pandas_ta.utils import verify_series
 
-def wnr(data, n=None):
+def wnr(close, low, high, n=None):
+    
+    # Validate arguments
+    
+    close = verify_series(close)
+    low = verify_series(low)
+    high = verify_series(high)
+
+    if close is None: return
+    if low is None: return
+    if open is None: return    
     n = n if n and n > 0 else 14
 
-
+    #calculate
+    
     high_prices = []
     low_prices = []
     WNR = []
 
-    for index, row in data.iterrows():
-        high_prices.append(row["high"])
+    for i in range(0,len(close)):
+        high_prices.append(high[i])
         if len(high_prices) == n:
             del high_prices[0]
-        low_prices.append(row["low"])
+        low_prices.append(low[i])
         if len(low_prices) == n:
             del low_prices[0]
 
         highest = max(high_prices)
         lowest = min(low_prices)
 
-        wnr = (highest - row["close"]) / (highest - lowest) * 100
+        wnr = (highest - close[i]) / (highest - lowest) * 100
         WNR.append(wnr)
+    # Prepare DataFrame to return
+    WNR = pd.DataFrame(WNR)    
+    WNR.category = "trend"
+    WNR.name = "Williams_Index"
 
     return WNR
+
