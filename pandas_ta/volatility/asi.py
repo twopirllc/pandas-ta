e3c2d7e4131008dc37ce68857bf60d9d68256317
andymc31 <72295085+andymc31@users.noreply.github.com>
2023-02-11 15:45:18 -0500

    Update asi.py

--- pandas_ta/volatility/asi.py
+++ pandas_ta/volatility/asi.py
@@ -1,48 +1,65 @@
-# -*- coding: utf-8 -*-
 """
 Indicator: ASI Accumulation Swing Index
 Inspired by tushare
+
 """
 import numpy as np
 from pandas_ta.overlap import ma
+import pandas as pd
+from pandas_ta.utils import verify_series
 
-def asi(data, n=None):
-
+def asi(close, low, high, open, n=None):
+    
+    # Validate arguments
+    
+    close = verify_series(close)
+    low = verify_series(low)
+    high = verify_series(high)
+    open = verify_series(open)
+    
+    if close is None: return
+    if low is None: return
+    if high is None: return    
+    if open is None: return  
+    
     n = n if n and n > 0 else 5
-
+    #calculate
     SI = []
-    for index, row in data.iterrows():
-        if index == 0:
-            last_row = row
-            SI.append(0.)
+    SI.append(0.)
+
+    for i in range(1,len(close)):
+        a = abs(close[i] - close[i-1])
+        b = abs(low[i] - close[i-1])
+        c = abs(high[i] - close[i-1])
+        d = abs(close[i-1] - open[i-1])
+
+        if b > a and b > c:
+            r = b + (1 / 2) * a + (1 / 4) * d
+        elif c > a and c > b:
+            r = c + (1 / 4) * d
         else:
+            r = 0
 
-            a = abs(row["close"] - last_row["close"])
-            b = abs(row["low"] - last_row["close"])
-            c = abs(row["high"] - last_row["close"])
-            d = abs(last_row["close"] - last_row["open"])
+        e = close[i] - close[i-1]
+        f = close[i] - open[i-1]
+        g = close[i-1] - open[i-1]
 
-            if b > a and b > c:
-                r = b + (1 / 2) * a + (1 / 4) * d
-            elif c > a and c > b:
-                r = c + (1 / 4) * d
-            else:
-                r = 0
+        x = e + (1 / 2) * f + g
+        k = max(a, b)
+        l = 3
 
-            e = row["close"] - last_row["close"]
-            f = row["close"] - last_row["open"]
-            g = last_row["close"] - last_row["open"]
+        if np.isclose(r, 0) or np.isclose(l, 0):
+            si = 0
+        else:
+            si = 50 * (x / r) * (k / l)
 
-            x = e + (1 / 2) * f + g
-            k = max(a, b)
-            l = 3
+        SI.append(si)
 
-            if np.isclose(r, 0) or np.isclose(l, 0):
-                si = 0
-            else:
-                si = 50 * (x / r) * (k / l)
+    # Prepare DataFrame to return
+    SI = pd.DataFrame(SI)
 
-            SI.append(si)
-
-    ASI = ma("sma",SI, n)
+    ASI = SI.rolling(n).mean()
+    ASI.name = 'ASI'
+    ASI.category = 'volatility'
     return ASI
+
