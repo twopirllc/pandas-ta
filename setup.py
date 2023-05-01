5cd0121e883397cc0307697b4c68b33e71b62f9d
Kevin Johnson <appliedmathkj@gmail.com>
2023-05-01 13:13:45 -0700

    MAINT #644 zlma refactor and version update

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
         "pandas_ta.volatility",
         "pandas_ta.volume"
     ],
-    version=".".join(("0", "3", "87b")),
+    version=".".join(("0", "3", "88b")),
     description=long_description,
     long_description=long_description,
     author="Kevin Johnson",
