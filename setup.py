39e51afb8d93a30dccea87644fae8f0b71ffe4e9
Kevin Johnson <appliedmathkj@gmail.com>
2023-11-14 15:22:56 -0800

    DOC MAINT #729 README update, example notebook -> dev folder

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
         "pandas_ta.volatility",
         "pandas_ta.volume"
     ],
-    version=".".join(("0", "4", "4b")),
+    version=".".join(("0", "4", "5b")),
     description=long_description,
     long_description=long_description,
     author="Kevin Johnson",
