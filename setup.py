00700c4088176caca74e031a414bac14fce9604b
Kevin Johnson <appliedmathkj@gmail.com>
2023-11-29 13:24:39 -0800

    DOC ENH #740 mp chunksize fix

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
         "pandas_ta.volatility",
         "pandas_ta.volume"
     ],
-    version=".".join(("0", "4", "6b")),
+    version=".".join(("0", "4", "7b")),
     description=long_description,
     long_description=long_description,
     author="Kevin Johnson",
